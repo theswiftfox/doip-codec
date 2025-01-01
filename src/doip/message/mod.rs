aed1c6e4db078b0a74a22a3ad134122bb4bae31e
Samuel Preston <samp.reston@outlook.com>
2025-01-01 18:32:32 +0000

    create size of tests to debug size of structs

--- src/doip/message/mod.rs
+++ src/doip/message/mod.rs
@@ -8,3 +8,29 @@ pub mod nack_codes;
 pub mod node_type;
 pub mod power_mode;
 pub mod sync_status;
+
+#[cfg(test)]
+mod size_tests {
+    use std::mem;
+
+    use crate::doip::message::{
+        action_code::ActionCode, activation_code::ActivationCode, activation_type::ActivationType,
+        diagnostic_ack::DiagnosticAckCode, diagnostic_nack::DiagnosticNackCode,
+        message::DoipMessage, nack_codes::NackCodes, node_type::NodeType, power_mode::PowerMode,
+        sync_status::SyncStatus,
+    };
+
+    #[test]
+    fn test_struct_sizes() {
+        dbg!(mem::size_of::<ActionCode>());
+        dbg!(mem::size_of::<ActivationCode>());
+        dbg!(mem::size_of::<ActivationType>());
+        dbg!(mem::size_of::<DiagnosticAckCode>());
+        dbg!(mem::size_of::<DiagnosticNackCode>());
+        dbg!(mem::size_of::<DoipMessage>());
+        dbg!(mem::size_of::<NackCodes>());
+        dbg!(mem::size_of::<NodeType>());
+        dbg!(mem::size_of::<PowerMode>());
+        dbg!(mem::size_of::<SyncStatus>());
+    }
+}
